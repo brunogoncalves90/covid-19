907a1e157e14dd6b73adbfe100f14d666494a1ba
brunogoncalves90 <44438843+brunogoncalves90@users.noreply.github.com>
2020-04-17 15:42:02 -0300

    Add files via upload

---
+++ country_cleaner.py
@@ -0,0 +1,20 @@
+  
+# country_cleaner.py
+"""
+A mapping from country names in the JHU/CSSE dataset to those used by the
+Wikipedia page for country populations.
+"""
+
+country_cleaner = {
+    'Cabo Verde': 'Cape Verde',
+    'Congo (Brazzaville)': 'Congo',
+    'Congo (Kinshasa)': 'DR Congo',
+    "Cote d'Ivoire": 'Ivory Coast',
+    'Czechia': 'Czech Republic',
+    'Holy See': 'Vatican City',
+    'Korea, South': 'South Korea',
+    'Taiwan*': 'Taiwan',
+    'US': 'United States',
+    'Timor-Leste': 'East Timor',
+    'West Bank and Gaza': 'Palestine',
+}
